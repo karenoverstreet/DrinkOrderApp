50447ecdcb802b9948589ec0855153a8042a7f98
Sarah Duitsman <sksmart9@gmail.com>
2016-07-05 22:05:30 -0500

    Updates to align with worksheet changes based on feedback

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -2,31 +2,41 @@
 var orderCount = 0;
 
 /* array of drink objects
-   each object has a value key with a camelCase drink name value 
-   and a label key with the display text for the drink */
+   each object has an id key with a camelCase drink id value 
+   and a label key with a value of the display text for the drink */
 
 // loadMenu function to add the drinks from the array to the page
-
 
 // submitOrder function that increments orderCount, gathers and displays order details.
 var submitOrder = function() {
   orderCount++;
   var orderName = $("#order-form-input").val();
   console.log(orderName);
-  var drinkName = $("label[for='" + $("input[type='radio']:checked").attr("id") + "']").text();
+  var drinkName = $("input[type='radio']:checked").val();
   
-  // only add orders if there is room left on the order queue
-  if (orderCount <= 5) {
-    // add order display
-    $("#order-details").append("<h1>" + orderName + " would like a " + drinkName + "</h1>");
+  // only add the order if a name has been entered
+  if (orderName) {
+    // only add orders if there is room left on the order queue
+    if (orderCount <= 5) {
+      // add order display
+      $("#order-details").append("<h1>" + orderName + " would like a " + drinkName + "</h1>");
 
-    // update drink order count
-    $('#drink-count').html("Drinks Ordered: " + orderCount);
+      // call funciton to update drink order count
+      updateOrderCount(orderCount);
+    }
+    else {
+      alert('Drink order queue is full.  Please try ordering again in a few minutes.');
+    }
   }
-  else {
-    alert('Drink order queue is full.  Please try ordering again in a few minutes.');
+  else { // if orderName has no value trigger an alert
+    alert('Oops! Please enter your name to order your drink.');
   }
 };
+
+// updateOrderCount function with a parameter passed to itf
+var updateOrderCount = function(count) { 
+  $('#drink-count').html("Drinks Ordered: " + count); 
+}
 
 
 $(document).ready(function() {
