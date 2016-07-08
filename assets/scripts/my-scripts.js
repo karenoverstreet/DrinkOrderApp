c7eacdfbc5606719f2d88a6ca2a5e0940badf349
Sarah Duitsman <sksmart9@gmail.com>
2016-07-07 21:54:55 -0500

    Add worksheet refs to comments

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,49 +1,42 @@
-//initialize order count
+// initialize order count (Worksheet Part 1: Step 9a)
 var orderCount = 0;
 
-/* array of drink objects
-   each object has an id key with a camelCase drink id value 
-   and a label key with a value of the display text for the drink */
+/* array of drink objects (Worksheet Part 2: Step 1)
+   each object has a value key with a camelCase drink name value 
+   and a label key with the display text for the drink */
 
-// loadMenu function to add the drinks from the array to the page
+// loadMenu function to add the drinks from the array to the page (Worksheet Part 2: Step 2)
 
-// submitOrder function that increments orderCount, gathers and displays order details.
+/* submitOrder function that increments orderCount, gathers and displays order details.
+   (Worksheet Part 1: Step 3) */
 var submitOrder = function() {
   orderCount++;
   var orderName = $("#order-form-input").val();
   console.log(orderName);
   var drinkName = $("input[type='radio']:checked").val();
   
-  // only add the order if a name has been entered
-  if (orderName) {
-    // only add orders if there is room left on the order queue
-    if (orderCount <= 5) {
-      // add order display
-      $("#order-details").append("<h1>" + orderName + " would like a " + drinkName + "</h1>");
+  // only add orders if there is room left on the order queue (Worksheet Part 1: Step 9g)
+  if (orderCount <= 5) {
+	// add order display (Worksheet Part 1: Step 7)
+	$("#order-details").append("<h1>" + orderName + " would like a " + drinkName + "</h1>");
 
-      // call funciton to update drink order count
-      updateOrderCount(orderCount);
-    }
-    else {
-      alert('Drink order queue is full.  Please try ordering again in a few minutes.');
-    }
+	// call funciton to update drink order count (Worksheet Part 1: Step 9e)
+	updateOrderCount(orderCount);
   }
-  else { // if orderName has no value trigger an alert
-    alert('Oops! Please enter your name to order your drink.');
+  else {
+	alert('Drink order queue is full.  Please try ordering again in a few minutes.');
   }
-};
 
-// updateOrderCount function with a parameter passed to itf
+// updateOrderCount function with a parameter passed to it (Worksheet Part 1: Step 9c)
 var updateOrderCount = function(count) { 
   $('#drink-count').html("Drinks Ordered: " + count); 
 }
 
-
 $(document).ready(function() {
-  // call loadMenu function
+  // call loadMenu function (Worksheet Part 2: Step 2d)
 
   $('#order-btn').click(function() {
-    // call submitOrder function when order button is clicked
+    // call submitOrder function when order button is clicked (Worksheet Part 1: Step 5b)
     submitOrder();
   });
 });
